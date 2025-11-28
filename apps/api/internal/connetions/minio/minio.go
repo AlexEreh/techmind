620ff3f761db77306df8447ad121030027258094
zlymix <veryzlymix@gmail.com>
2025-11-29 00:33:00 +0300

    autocreate minio bucket

--- apps/api/internal/connetions/minio/minio.go
+++ apps/api/internal/connetions/minio/minio.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"techmind/pkg/config"
 
+	"log"
+
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 	"go.uber.org/fx"
@@ -16,6 +18,24 @@ func New(lc fx.Lifecycle, config *config.Config) *minio.Client {
 	})
 	if err != nil {
 		panic(err)
+	}
+
+	// Auto-create documents bucket if it doesn't exist
+	ctx := context.Background()
+	bucketName := "documents"
+
+	exists, err := client.BucketExists(ctx, bucketName)
+	if err != nil {
+		log.Printf("Error checking if bucket exists: %v", err)
+	}
+
+	if !exists {
+		err = client.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{})
+		if err != nil {
+			log.Printf("Error creating bucket: %v", err)
+		} else {
+			log.Printf("Bucket '%s' created successfully", bucketName)
+		}
 	}
 
 	lc.Append(fx.Hook{
